cf398aafaf4c32ffda33c71f698261d51bb40b81
Alejandro Hernandez <alejandrohernandez996@gmail.com>
2021-09-10 03:54:36 -0500

    bg task

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,32 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask
 from flask_socketio import SocketIO
-from flask_celery import make_celery
+from stockData import StockData
+from cryptoData import CryptoData
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 
-app.config.update(
-    CELERY_BROKER_URL = 'amqp://localhost//',
-    CELERY_RESULT_BACKEND='rpc://'
-)
-
-celery = make_celery(app)
 socketio = SocketIO(app)
+rooms = {}
+def bg_emit():
+    for room in rooms:
+        stockData = StockData(room)
+        cryptoData = StockData(room)
+        price = stockData.getCurrentPrice()
+        if price is None:
+            price = cryptoData.getCurrentPrice()
+        socket.io.emit('price',{'price' : price} , room=room)    
+def listen():
+    while True:
+        bg_emit()
+        eventlet.sleep(2)
+
+eventlet.spawn(listen)
 
 from app import routes, events
 
 if __name__ == '__main__':
-    socketio.run(app,message_queue='amqp://',cors_allowed_origins='http://fincha.tv',host='137.184.101.17', port=80)
+    socketio.run(app,cors_allowed_origins='http://fincha.tv',host='137.184.101.17', port=80)
--- app/events.py
+++ app/events.py
@@ -1,32 +1,10 @@
 from flask import session
 from flask_socketio import emit, join_room, leave_room
 from app import app
-from app.stockData import StockData
-from app.cryptoData import CryptoData
 from . import socketio
 import random
 import string
 import time
-
-
-room_set = {}
-
-@app.celery.task(name='task.message')
-def 
-s = sched.scheduler(time.time, time.sleep)
-def emit_prices(sc): 
-    for room in room_set:
-        print('yo')
-        stockData = StockData(room)
-        cryptoData = StockData(room)
-        price = stockData.getCurrentPrice()
-        if price is None:
-            price = cryptoData.getCurrentPrice()
-        emit('price',{'price' : price} , room=room)    
-    s.enter(2, 1, emit_prices, (sc,))
-
-s.enter(2, 1, emit_prices, (s,))
-s.run()
 
 @socketio.on('joined', namespace='/chat')
 def joined(data):
@@ -36,7 +14,7 @@ def joined(data):
     join_room(room)
     emit('status', {'msg': name + ' has entered the room.'}, room=room)
     session['name'] = name
-    room_set.add(room)
+    app.rooms.add(room)
 
 @socketio.on('text', namespace='/chat')
 def text(data):
